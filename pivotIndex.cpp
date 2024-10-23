3be44bffa3a68e50e262b17a6cb6289735c82e7f
manahil <manahiliqbal0511@gmail.com>
2024-10-24 00:44:42 +0500

    Pivot Index

---
+++ pivotIndex.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total_sum = 0;
+        int left_sum = 0;
+    
+        for (int num : nums) {
+            total_sum += num;
+        }
+    
+        for (int i = 0; i < nums.size(); ++i) {
+            // Right sum would be total_sum - left_sum - nums[i]
+            if (left_sum == total_sum - left_sum - nums[i]) {
+                return i;
+            }
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+
+};
