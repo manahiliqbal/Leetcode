7db16d0b265c95fb2dbcb783554cb72465cbfb9d
manahil <manahiliqbal0511@gmail.com>
2024-12-29 22:12:07 +0500

    2215. Find the Difference of Two Arrays

---
+++ differenceOfTwoArrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+    
+        vector<int> diff1, diff2;
+
+        for (int num : set1) {
+            if (set2.find(num) == set2.end()) {
+                diff1.push_back(num);
+            }
+        }
+
+        for (int num : set2) {
+            if (set1.find(num) == set1.end()) {
+                diff2.push_back(num);
+            }
+        }
+        return {diff1, diff2};
+    }
+};
